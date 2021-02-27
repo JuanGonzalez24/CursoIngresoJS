b4153d7004ab99a429c796ca34315ce911a27fa1
JuanGonzalez24 <78236687+JuanGonzalez24@users.noreply.github.com>
2021-02-27 10:29:43 -0300

    new commit

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,56 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	var mesDelAño; 
+
+	mesDelAño=txtIdMes.value;
+	
+	switch (mesDelAño)
+	{
+		case "Enero" : 
+			alert("Que comiences bien el año");
+			break;
+		case "Marzo" :
+			alert("a clase");
+			break;
+		case "Julio" :
+			alert("se vienen las vacas");
+			break;
+		case "Diciembre" : 
+			alert("Felices fiesta!!!.");
+			break;			
+	}
+
+	
 
 
 
 }//FIN DE LA FUNCIÓN
+
+
+/*if(mesDelAño == "Enero")
+	{	
+		alert("Que comiences bien el año");
+	}	
+	else
+	{	
+		if(mesDelAño == "Marzo")
+		{
+			alert("a clase");
+		}	
+		else
+		{
+			if(mesDelAño == "Julio")
+			{	
+				alert("Se vienen las vaciones");
+			}	
+			else
+			}	
+				if
+
+			}
+		}	
+
+
+	}
+*/
